52536489dd795f6a941095d68ac8c3ac14598e72
Seunghyun-Kang <kiseyno@gmail.com>
2022-03-30 16:43:43 +0900

    Change getOptPortfolio API

--- app/src/app/services/request.service.ts
+++ app/src/app/services/request.service.ts
@@ -54,13 +54,8 @@ export class RequestService {
     );
   }
 
-  getOptPortfolio(codes: string[]) {
-    var array = ""
-    codes.forEach((element, index) => {
-      array += element
-      if(index < codes.length-1) array += ',' 
-    });
-    let payload = {codes: array}
+  getOptPortfolio(codes: string) {
+    let payload = {codes: codes}
     return this.http.get(this.REST_SERVER_URL + 'optimalportfolio/', { observe: 'response', headers: this.headers, params: payload })
     .pipe(
       catchError(this.handleError)
