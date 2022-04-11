a6098e918c506cd226d604b5691d6865e9af0ef2
seun9.kang <seun9.kang@samsung.com>
2022-04-11 15:14:18 +0900

    Add GET latest signals in dashboard component

--- app/src/app/components/dashboard/dashboard.component.ts
+++ app/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PagestatusService } from 'src/app/services/pagestatus.service';
+import { RequestService } from 'src/app/services/request.service';
+import { signalData } from '../stockdetail/stockdetail.model';
 
 @Component({
   selector: 'app-menu',
@@ -16,8 +18,13 @@ export class DashboardComponent implements OnInit {
     "특정 종목 차트와 매매 알고리즘을 보여주는 기능",
     "선택해봐"
   ]
+  public rawLatestSignalTrend: signalData[] = []
+  public rawLatestSignalReverse: signalData[] = []
+  public rawLatestSignalTripleScreen: signalData[] = []
+
   constructor(private statusService: PagestatusService,
-    private router: Router) {
+    private router: Router,
+    private requestService: RequestService) {
     this.statusService.setStatus("loading-forward")
   }
 
@@ -27,6 +34,30 @@ export class DashboardComponent implements OnInit {
       this.getData = true
     }, 1000);
     console.log(this.guideIndex)
+
+    this.requestService.getLatestBollingerTrendSignal()
+    .subscribe({
+      next: (v: any) => {
+        this.rawLatestSignalTrend = Object(v.body)
+        },
+      error: (e: any) => console.log("ERROR OCCURED :: " + JSON.stringify(e))
+    });
+
+    this.requestService.getLatestBollingerReverseSignal()
+    .subscribe({
+      next: (v: any) => {
+        this.rawLatestSignalReverse = Object(v.body)
+        },
+      error: (e: any) => console.log("ERROR OCCURED :: " + JSON.stringify(e))
+    });
+
+    this.requestService.getLatestTripleScreenSignal()
+    .subscribe({
+      next: (v: any) => {
+        this.rawLatestSignalTripleScreen = Object(v.body)
+        },
+      error: (e: any) => console.log("ERROR OCCURED :: " + JSON.stringify(e))
+    });
   }
 
   guideIndexChanged(event: any) {
