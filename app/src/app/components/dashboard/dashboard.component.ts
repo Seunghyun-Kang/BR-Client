724acdc8d28921ffad89b680a9fdd9b87aaaa844
seun9.kang <seun9.kang@samsung.com>
2022-04-11 15:16:37 +0900

    Remove auto guide in dashboard

--- app/src/app/components/dashboard/dashboard.component.ts
+++ app/src/app/components/dashboard/dashboard.component.ts
@@ -12,12 +12,6 @@ import { signalData } from '../stockdetail/stockdetail.model';
 export class DashboardComponent implements OnInit {
   public getData: boolean = false
   public status = "loading-forward";
-  public guideIndex: number = -1;
-  public guideist = [
-    "너가 선택한 주식들의 최적화 포트폴리오가 어떤 비율인지 계산하는 기능",
-    "특정 종목 차트와 매매 알고리즘을 보여주는 기능",
-    "선택해봐"
-  ]
   public rawLatestSignalTrend: signalData[] = []
   public rawLatestSignalReverse: signalData[] = []
   public rawLatestSignalTripleScreen: signalData[] = []
@@ -33,7 +27,6 @@ export class DashboardComponent implements OnInit {
       this.statusService.setStatus("normal")
       this.getData = true
     }, 1000);
-    console.log(this.guideIndex)
 
     this.requestService.getLatestBollingerTrendSignal()
     .subscribe({
@@ -58,11 +51,6 @@ export class DashboardComponent implements OnInit {
         },
       error: (e: any) => console.log("ERROR OCCURED :: " + JSON.stringify(e))
     });
-  }
-
-  guideIndexChanged(event: any) {
-    this.guideIndex = event
-    console.log(this.guideIndex)
   }
 
   onPressGetStock() {
