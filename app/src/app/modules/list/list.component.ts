dcbe9d2d5bf3e9fbe3b607152acfcda4548c1a19
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-14 02:09:36 +0900

    Add list module

---
+++ app/src/app/modules/list/list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit ,Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+  public columnNum: number = 0
+  public columnArray: string[] = []
+  public rowArray: Array<string[]> = []
+  public title: string = ""
+
+  constructor(
+  @Inject(MAT_DIALOG_DATA) public data: {title: string, column: string[], data: Array<string[]>},
+  private router: Router,
+  private dialogRef: MatDialog) { 
+
+  }
+  
+  ngOnInit(): void {
+    this.columnArray = this.data.column
+    this.rowArray = this.data.data
+    this.title = this.data.title
+    this.columnNum = this.data.column.length
+  }
+
+  onTapCompany(code: string, company: string) {
+    this.dialogRef.closeAll()
+    this.router.navigate(['stockdetail'], {
+      queryParams: {
+        code: code,
+        companyName: company
+      }
+    })
+  }
+
+}
