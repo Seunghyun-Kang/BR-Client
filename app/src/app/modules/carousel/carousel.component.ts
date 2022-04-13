32ee19369f8188f4b501c3bac6572713f135d35d
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-14 02:29:38 +0900

    Add condition of autoslide by Mobile or not

--- app/src/app/modules/carousel/carousel.component.ts
+++ app/src/app/modules/carousel/carousel.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 
+const IS_MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
@@ -8,7 +10,7 @@ import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 })
 export class CarouselComponent implements OnInit {
   @Input() inputs: any[] = []
-  @Input() speed: number = 5000
+  @Input() speed: number = IS_MOBILE? 0: 5000
   @ViewChild(NgbCarousel) carousel;
   
   constructor() {
