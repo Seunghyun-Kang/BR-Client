592ad3c6cb0de9ccbf9941dd09202542b444d202
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-14 02:09:57 +0900

    Add dialog in dashboard

--- app/src/app/components/dashboard/dashboard.component.ts
+++ app/src/app/components/dashboard/dashboard.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IgxNavigationDrawerComponent } from 'igniteui-angular';
 import { DataService } from 'src/app/services/data.service';
 import { PagestatusService } from 'src/app/services/pagestatus.service';
 import { RequestService } from 'src/app/services/request.service';
 import { signalData } from '../stockdetail/stockdetail.model';
-
+import { MatDialog } from '@angular/material/dialog';
+import { ListComponent } from 'src/app/modules/list/list.component';
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-menu',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-  @ViewChild(IgxNavigationDrawerComponent, { static: true })
-  public drawer: IgxNavigationDrawerComponent;
 
+export class DashboardComponent implements OnInit {
   public getTrendData: boolean = false
   public getReverseData: boolean = false
   public getTripleScreenData: boolean = false
@@ -34,11 +33,13 @@ export class DashboardComponent implements OnInit {
   constructor(private statusService: PagestatusService,
     private router: Router,
     private requestService: RequestService,
-    private dataService: DataService) {
+    private dataService: DataService,
+    public dialog: MatDialog) {
     this.statusService.setStatus("loading-forward")
   }
 
   ngOnInit(): void {
+
     setTimeout(() => {
       this.statusService.setStatus("normal")
     }, 1000);
@@ -50,44 +51,7 @@ export class DashboardComponent implements OnInit {
           console.log(this.rawLatestSignalTrend)
           this.parseSignalData(this.rawLatestSignalTrend, "bollinger-trend")
         },
-        error: (e: any) => {
-          //     this.rawLatestSignalTrend = [{code: '001780', date: '2022-04-11', type: 'sell', close: 3755, valid: 'valid'},
-          //   {code: '004370', date: '2022-04-11', type: 'buy', close: 319500, valid: 'valid'},
-          //  {code: '007210', date: '2022-04-11', type: 'sell', close: 3475, valid: 'valid'},
-          //  {code: '007820', date: '2022-04-11', type: 'sell', close: 6640, valid: 'valid'},
-          //   {code: '008250', date: '2022-04-11', type: 'sell', close: 11900, valid: 'valid'},
-          //  {code: '013700', date: '2022-04-11', type: 'sell', close: 2395, valid: 'valid'},
-          //  {code: '015890', date: '2022-04-11', type: 'buy', close: 7210, valid: 'valid'},
-          //  {code: '025550', date: '2022-04-11', type: 'sell', close: 6180, valid: 'valid'},
-          //  {code: '026150', date: '2022-04-11', type: 'sell', close: 11350, valid: 'valid'},
-          //  {code: '028050', date: '2022-04-11', type: 'sell', close: 24350, valid: 'valid'},
-          //  {code: '029780', date: '2022-04-11', type: 'buy', close: 33050, valid: 'valid'},
-          //  {code: '035890', date: '2022-04-11', type: 'sell', close: 1815, valid: 'valid'},
-          //   {code: '036010', date: '2022-04-11', type: 'buy', close: 11650, valid: 'valid'},
-          //   {code: '036090', date: '2022-04-11', type: 'sell', close: 1295, valid: 'valid'},
-          //   {code: '038010', date: '2022-04-11', type: 'sell', close: 5850, valid: 'valid'},
-          //  {code: '040910', date: '2022-04-11', type: 'sell', close: 10400, valid: 'valid'},
-          //  {code: '043340', date: '2022-04-11', type: 'buy', close: 1290, valid: 'valid'},
-          //  {code: '043360', date: '2022-04-11', type: 'buy', close: 3830, valid: 'valid'},
-          //  {code: '046210', date: '2022-04-11', type: 'buy', close: 3920, valid: 'valid'},
-          //   {code: '046940', date: '2022-04-11', type: 'sell', close: 5340, valid: 'valid'},
-          //   {code: '047560', date: '2022-04-11', type: 'sell', close: 13800, valid: 'valid'},
-          //   {code: '054040', date: '2022-04-11', type: 'sell', close: 4715, valid: 'valid'},
-          //   {code: '058110', date: '2022-04-11', type: 'sell', close: 7280, valid: 'valid'},
-          //   {code: '066430', date: '2022-04-11', type: 'buy', close: 3235, valid: 'valid'},
-          //   {code: '082210', date: '2022-04-11', type: 'buy', close: 7210, valid: 'valid'},
-          //  {code: '084650', date: '2022-04-11', type: 'sell', close: 9540, valid: 'valid'},
-          //  {code: '085670', date: '2022-04-11', type: 'buy', close: 6260, valid: 'valid'},
-          //  {code: '089600', date: '2022-04-11', type: 'sell', close: 28050, valid: 'valid'},
-          //   {code: '101170', date: '2022-04-11', type: 'sell', close: 4825, valid: 'valid'},
-          //  {code: '102120', date: '2022-04-11', type: 'buy', close: 13550, valid: 'valid'},
-          //  {code: '105330', date: '2022-04-11', type: 'sell', close: 10850, valid: 'valid'},
-          //  {code: '107590', date: '2022-04-11', type: 'buy', close: 147500, valid: 'valid'},
-          //   {code: '117730', date: '2022-04-11', type: 'sell', close: 8660, valid: 'valid'},
-          //   {code: '121800', date: '2022-04-11', type: 'sell', close: 16850, valid: 'valid'}
-          //   ]
-          //   this.getTrendData = true
-        }
+        error: (e: any) => { }
       });
 
     this.requestService.getLatestBollingerReverseSignal()
@@ -109,6 +73,126 @@ export class DashboardComponent implements OnInit {
         },
         error: (e: any) => console.log("ERROR OCCURED :: " + JSON.stringify(e))
       });
+  }
+
+  openDialog(type: string) {
+    let dataArray: Array<string[]> = []
+    let title = ""
+    switch (type) {
+      case "buyTrend":
+        this.rawLatestSignalTrend.forEach(element => {
+          title = "볼린저 추세 추동 매수 신호"
+          if(element.type === "buy" && element.valid === "valid"){
+            dataArray.push([
+              this.dataService.getCompanyNamebyCode(element.code),
+              element.date,
+              String(element.close),
+              element.last_sell_date,
+              String(element.last_sell_close),
+              "-",
+              element.code
+            ])
+          }
+        });
+        break;
+      case "buyReverse":
+        title = "볼린저 반전 매매 매수 신호"
+        this.rawLatestSignalReverse.forEach(element => {
+          if(element.type === "buy" && element.valid === "valid"){
+            dataArray.push([
+              this.dataService.getCompanyNamebyCode(element.code),
+              element.date,
+              String(element.close),
+              element.last_sell_date,
+              String(element.last_sell_close),
+              "-",
+              element.code
+            ])
+          }
+        });
+        break;
+      case "buyTriple":
+        title = "삼중창 매수 신호"
+
+        this.rawLatestSignalTripleScreen.forEach(element => {
+          if(element.type === "buy" && element.valid === "valid"){
+            dataArray.push([
+              this.dataService.getCompanyNamebyCode(element.code),
+              element.date,
+              String(element.close),
+              element.last_sell_date,
+              String(element.last_sell_close),
+              "-",
+              element.code
+            ])
+          }
+        });
+        break;
+      case "sellTrend":
+        title = "볼린저 추세 추동 매도 신호"
+
+        this.rawLatestSignalTrend.forEach(element => {
+          if(element.type === "sell" && element.valid === "valid"){
+            dataArray.push([
+              this.dataService.getCompanyNamebyCode(element.code),
+              element.date,
+              String(element.close),
+              element.last_buy_date,
+              String(element.last_buy_close),
+              String(((element.close - element.last_buy_close) / element.last_buy_close * 100).toFixed(2)),
+              element.code
+            ])
+          }
+        });
+        break;
+      case "sellReverse":
+        title = "볼린저 반전 매매 매도 신호"
+
+        this.rawLatestSignalReverse.forEach(element => {
+          if(element.type === "sell" && element.valid === "valid"){
+            dataArray.push([
+              this.dataService.getCompanyNamebyCode(element.code),
+              element.date,
+              String(element.close),
+              element.last_buy_date,
+              String(element.last_buy_close),
+              String(((element.close - element.last_buy_close) / element.last_buy_close * 100).toFixed(2)) + '%',
+              element.code
+            ])
+          }
+        });
+        break;
+      case "sellTriple":
+        title = "볼린저 추세 추동 매도 신호"
+
+        this.rawLatestSignalTripleScreen.forEach(element => {
+          if(element.type === "sell" && element.valid === "valid"){
+            dataArray.push([
+              this.dataService.getCompanyNamebyCode(element.code),
+              element.date,
+              String(element.close),
+              element.last_buy_date,
+              String(element.last_buy_close),
+              String(((element.close - element.last_buy_close) / element.last_buy_close * 100).toFixed(2)) + '%',
+              element.code
+            ])
+          }
+        });
+        break;
+
+      default:
+        break
+    }
+
+    this.dialog.open(ListComponent, {
+      width:'80vw',
+      height:'80vh',
+      data: {
+        title: title,
+        column: ["종목",  "신호일", "신호가","과거 거래일", "과거 거래가", "수익률"],
+        data: dataArray
+      },
+    });
   }
 
   parseSignalData(data: signalData[], type: string) {
