50bea297691033bc657a6c8a22cb936d085778ee
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-14 17:00:08 +0900

    Add scroll focus in information component

--- app/src/app/components/information/information.component.ts
+++ app/src/app/components/information/information.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-information',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./information.component.scss']
 })
 export class InformationComponent implements OnInit {
+  @ViewChild('group') group;
 
+  public btnElement: Element;
+  public selectedIndex = 0
+  private itemLen = 3
+  ElByClassName: any;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  tabChanged(tabChangeEvent: MatTabChangeEvent): void {
+    this.selectedIndex = tabChangeEvent.index;
+}
+
+  onSwipeRight(event: any) {
+    console.log("RIGHT")
+    if(this.selectedIndex !== 0) {this.selectedIndex = this.selectedIndex - 1;this.group.focusTab(this.selectedIndex )}
+ 
+  }
+  onSwipeLeft(event: any) {
+    console.log("Left")
+    if(this.selectedIndex !== this.itemLen-1) {this.selectedIndex = this.selectedIndex + 1
+    this.group.focusTab(this.selectedIndex )
+    }
+  }
 }
