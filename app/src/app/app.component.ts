6f7aac8c59aecf10077c3a1f563be304007fbaf0
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-15 11:09:26 +0900

    Add menu icon

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -19,5 +19,6 @@ export class AppComponent {
     this.iconSerivce.addSvgIcon('List', '../assets/iamge/list2-black.svg', 'user-icons')
     this.iconSerivce.addSvgIcon('Close', '../assets/iamge/close-black.svg', 'user-icons')
     this.iconSerivce.addSvgIcon('Question', '../assets/iamge/question-black.svg', 'user-icons')
+    this.iconSerivce.addSvgIcon('Menu', '../assets/iamge/menu-black.svg', 'user-icons')
   }
 }
