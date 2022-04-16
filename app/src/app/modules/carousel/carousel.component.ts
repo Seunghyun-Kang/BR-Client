a14782ff4616ece130197a633fd968aa48f2acb9
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-16 18:29:38 +0900

    Fix carousel options

--- app/src/app/modules/carousel/carousel.component.ts
+++ app/src/app/modules/carousel/carousel.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { IgxCarouselComponent, IgxListComponent, IListItemClickEventArgs, ISlideEventArgs } from 'igniteui-angular';
 
 const IS_MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
@@ -10,13 +11,42 @@ const IS_MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini
 })
 export class CarouselComponent implements OnInit {
   @Input() inputs: any[] = []
-  @Input() speed: number = IS_MOBILE? 0: 5000
-  @ViewChild(NgbCarousel) carousel;
+  @Input() speed: number = IS_MOBILE? 0: 2000
+  // @ViewChild(NgbCarousel) carousel;
   
-  constructor() {
+  @ViewChild(IgxCarouselComponent, { static: true })
+  public carousel: IgxCarouselComponent;
+
+  @ViewChild(IgxListComponent, {static: true})
+  public list: IgxListComponent;
+
+  public currentIndex = 0;
+  
+  constructor(private eRef: ElementRef,
+    ) {
   }
 
   ngOnInit(): void {
+    // this.addSlides();
+    this.list.itemClicked.subscribe((args: IListItemClickEventArgs) => {
+        this.currentIndex = args.item.index;
+        this.carousel.select(this.carousel.get(this.currentIndex));
+    });
+
+    this.carousel.onSlideChanged.subscribe((args: ISlideEventArgs) => {
+        this.currentIndex = args.slide.index;
+        (<HTMLElement>this.eRef.nativeElement).querySelector('.selected').scrollTo()
+    });
+  }
+
+  ngAfterViewInit() {
+  }
+
+  public slideStyle(image: string) {
+    return  {
+        background: `url(${image})`,
+        backgroundSize: 'cover'
+    };
   }
 
   onSwipeRight(event: any) {
