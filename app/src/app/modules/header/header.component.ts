97299f1ac576baf07240d263ae6b7cee2bc76809
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-18 21:38:48 +0900

    Add symbol selection in header

--- app/src/app/modules/header/header.component.ts
+++ app/src/app/modules/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Location } from '@angular/common';
 import { PagestatusService } from 'src/app/services/pagestatus.service';
+import { MatRadioChange } from '@angular/material/radio';
 
 @Component({
   selector: 'header',
@@ -11,6 +12,7 @@ export class HeaderComponent implements OnInit {
 
   @Input() title: string = ""
   public status: string = ""
+  public types:string[] = ["코스닥", "나스닥 & 뉴욕"]
   constructor(private location: Location, private service: PagestatusService) { }
 
   ngOnInit(): void {
@@ -23,4 +25,14 @@ export class HeaderComponent implements OnInit {
     this.location.back();
   }
 
+  onTapGlobe(mrChange: MatRadioChange){
+    switch(mrChange.value) {
+      case 0:
+         this.service.setType('KRX')
+        break;
+      case 1:
+        this.service.setType('NASDAQ')
+        break;
+    }
+  }
 }
