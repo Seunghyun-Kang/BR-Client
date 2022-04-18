eef0ddbb1b345b4c066b81bb717c55dd90ce3ccc
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-19 01:54:38 +0900

    Add icons

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -11,17 +11,19 @@ export class AppComponent {
 
   
   constructor(private iconSerivce: IgxIconService){
-    this.iconSerivce.addSvgIcon('OptPortfolio','../assets/iamge/document-black.svg' , 'user-icons')
+    this.iconSerivce.addSvgIcon('OptPortfolio','../assets/iamge/percent-black.svg' , 'user-icons')
     this.iconSerivce.addSvgIcon('StockDetails', '../assets/iamge/chart-black.svg', 'user-icons')
-    this.iconSerivce.addSvgIcon('Dashboard', '../assets/iamge/homepage-black.svg', 'user-icons')
+    this.iconSerivce.addSvgIcon('Dashboard', '../assets/iamge/home2-black.svg', 'user-icons')
     this.iconSerivce.addSvgIcon('Rocket', '../assets/iamge/rocket2-black.svg', 'user-icons')
     this.iconSerivce.addSvgIcon('Back', '../assets/iamge/back-black.svg', 'user-icons')
     this.iconSerivce.addSvgIcon('List', '../assets/iamge/list2-black.svg', 'user-icons')
     this.iconSerivce.addSvgIcon('Close', '../assets/iamge/close-black.svg', 'user-icons')
-    this.iconSerivce.addSvgIcon('Question', '../assets/iamge/question-black.svg', 'user-icons')
+    this.iconSerivce.addSvgIcon('Question', '../assets/iamge/question2-black.svg', 'user-icons')
     this.iconSerivce.addSvgIcon('Menu', '../assets/iamge/menu-black.svg', 'user-icons')
     this.iconSerivce.addSvgIcon('QuestionLeft', '../assets/iamge/question-left-black.svg', 'user-icons')
-    this.iconSerivce.addSvgIcon('LatestSignal', '../assets/iamge/list3-black.svg', 'user-icons')
+    this.iconSerivce.addSvgIcon('LatestSignal', '../assets/iamge/filter-list-black.svg', 'user-icons')
     this.iconSerivce.addSvgIcon('Globe', '../assets/iamge/globe-black.svg', 'user-icons')
+    this.iconSerivce.addSvgIcon('Korea', '../assets/iamge/korea.svg', 'user-icons')
+    this.iconSerivce.addSvgIcon('USA', '../assets/iamge/usa.svg', 'user-icons')
   }
 }
