79490a69b7091d7415d0059578e70d25b0801e83
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-22 01:00:54 +0900

    Upload V23

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -136,7 +136,7 @@ const appRoutes: Routes = [
     HTMLCanvasElement,
     {
       provide: "Version",
-      useValue: 'BR V22',
+      useValue: 'BR V23',
     },
     {
       provide: HAMMER_GESTURE_CONFIG,
